ee06897b260eaebcd3ff808a1fb06abb9eeba0df
SonaliCS9445 <162159357+SonaliCS9445@users.noreply.github.com>
2025-03-23 21:02:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,21 +63,21 @@ gsap.from("#about-us img, #about-us-in",{
     }
 })
 
-gsap.from(".card",{
-    y:50,
-    opacity:0,
-    duration:1,
-    stagger: 0.4,
-    scrollTrigger:{
-        trigger:".card",
-        scroller:"body",
-         // markers:true,
-        start:"top 70%",
-        end:"top 65%",
-        scrub:1
+// gsap.from(".card",{
+//     y:50,
+//     opacity:0,
+//     duration:1,
+//     stagger: 0.4,
+//     scrollTrigger:{
+//         trigger:".card",
+//         scroller:"body",
+//          // markers:true,
+//         start:"top 70%",
+//         end:"top 65%",
+//         scrub:1
 
-    }
-})
+//     }
+// })
 gsap.from("#col1",{
     y:-70,
     x:-70,
